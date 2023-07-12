054e89b1ce6dfb0040253fe5753e6a2bc4a4b533
Luis <ariza.9485@gmail.com>
2023-07-12 15:38:21 -0500

    mejoras y funcionalidades extras.

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/region.type';
+
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit  {
 
-  public countries: Country[] = []
+  public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
-  constructor ( private countriesService: CountriesService  ) {}
 
-  searchByRegion( region: string ): void {
-    this.countriesService.searchRegion( region )
-    .subscribe( countries => {
-      this.countries = countries;
-    } )
+  constructor(private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
   }
 
+  searchByRegion(region: Region): void {
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion(region)
+    .subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
 }
